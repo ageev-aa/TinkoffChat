4ac7463f0ed38f1ac7d87ca7e65c9a03b6aefe14
Alexander <ageev-aa@yandex.ru>
2018-02-27 22:18:10 +0300

    gitignore test
    gitignore test

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        print(#function)
         super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +23,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print (#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print (#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print (#function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print (#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print (#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print (#function)
+    }
 
 }
 
