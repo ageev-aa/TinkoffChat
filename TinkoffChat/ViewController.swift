72e3451f1f50e1ec3252561cf9d8023d2741eb54
Alexander <ageev-aa@yandex.ru>
2018-03-11 19:55:03 +0300

    2nd homework

--- TinkoffChat/ViewController.swift
+++ TinkoffChat/ViewController.swift
@@ -8,43 +8,120 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    @IBOutlet weak var addPhotoButton: UIButton!
+    @IBOutlet weak var userPhotoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var editButton: UIView!
+    
+    @IBAction func addPhotoAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Choose image", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
+            self.openCamera()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
+            self.openGallary()
+        }))
+        
+        alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCamera()
+    {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera))
+        {
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else
+        {
+            let alert  = UIAlertController(title: "Error", message: "Camera is not avalible", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func openGallary()
+    {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            userPhotoImageView.image = image
+        }
+        else if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            userPhotoImageView.image = image
+        } else{
+            print("Something went wrong")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func editAction(_ sender: Any) {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        //print("\(#function): \(editButton.frame)") //unexpectedly found nil while unwrapping an Optional value
+        // в данном этапе view еще не существует, editButton = nil
+    }
+    
     override func viewDidLoad() {
-        print(#function)
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        imagePicker.delegate = self
+        print("\(#function): \(editButton.frame)")
         
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         print (#function)
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        print (#function)
+        super.viewDidAppear(animated)
+        userPhotoImageView.layer.cornerRadius = addPhotoButton.frame.height/2
+        addPhotoButton.layer.cornerRadius = addPhotoButton.frame.height/2
+        print("\(#function): \(editButton.frame)")
+        // отличаются, потому что во viewDidLoad данные берутся из сториборда. viewDidAppear вызывается после 
+        // того, как view появляется на экране (и размеры уже известны)
     }
     
     override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         print (#function)
     }
     
     override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         print (#function)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         print (#function)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
         print (#function)
     }
 
